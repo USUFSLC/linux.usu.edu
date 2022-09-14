e201496deeb0d829d4f1798f040b789d9d8b3c2b
Logan Hunt <loganhunt@simponic.xyz>
2022-09-14 16:14:50 -0600

    Finish 'can' authorization method

--- src/db/models/user/extern.lisp
+++ src/db/models/user/extern.lisp
@@ -15,24 +15,11 @@
             user)))))
 
 (defun can (user operation context)
-  (select (:*)
-    (from :user_context)
-    (inner-join :user_context_role :on (:= :user_context_role.user_context_id :user_context.id))
-    (inner-join :context_role_operation :on (:= :context_role_operation.context_role_id :user_context_role.context_role_id))
-    (inner-join :context_operation :on (:= :context_operation.id :context_role_operation.context_operation_id))))
-    ;;(where (:and (:= :operation operation)
-    ;;             (:= :user_id (object-id user))))))
-
-    
-    
-;;  (let ((query "select * from user_context join user_context_role on user_context_role.user_context_id=user_context.id join context_role_operation on context_role_operation.context_role_id=user_context_role.context_role_id join context_operation on context_operation.id=context_role_operation.context_operation_id where operation='create' and user_id=1 limit 1;")
-;;        (query (select (:operation)
-;;                 (:from user_context)
-;;                 (:join user_context_role on user_context_role.user_context_id=user_context.id)
-;;                 (:join context_role_operation on context_role_operation.context_role_id=user_context_role.context_role_id)
-;;                 (:join context_operation on context_operation.id=context_role_operation.context_operation_id)
-;;                 (:where (:operation operation)
-;;                         (:and (:user_id user-id)))))
-;;
-;;  ()
-;;
+  (with-db ()
+    (mito:select-dao 'user-context
+      (inner-join :user_context_role :on (:= :user_context_role.user_context_id :user_context.id))
+      (inner-join :context_role_operation :on (:= :context_role_operation.context_role_id :user_context_role.context_role_id))
+      (inner-join :context_operation :on (:= :context_operation.id :context_role_operation.context_operation_id))
+      (where (:and (:= :context_operation.operation operation)
+                   (:= :user_context.context_id (object-id context))
+                   (:= :user_context.user_id (object-id user)))))))
