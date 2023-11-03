8992c40b457b6db96b74a2250d96a45bc25f08c5
Elizabeth Hunt <elizabeth.hunt@simponic.xyz>
2023-11-03 17:49:31 -0600

    update formatting for streams controller and update ffmpeg nginx

--- src/web/controllers/streams.lisp
+++ src/web/controllers/streams.lisp
@@ -1,6 +1,11 @@
 (in-package :usufslc.web)
 (syntax:use-syntax :annot)
 
+(defun current-year ()
+  (local-time:with-decoded-timestamp (:year y)
+                                     (local-time:now)
+                                     y))
+
 @route GET "/stream/create"
 (defun create-stream ()
   (usufslc.db:with-db
@@ -12,11 +17,6 @@
                           :env `(:csrf-token ,(lack.middleware.csrf:csrf-token *session*))
                           :root-env (root-env
                                       :page-title "New Stream"))))))
-
-(defun current-year ()
-  (local-time:with-decoded-timestamp (:year y)
-                                     (local-time:now)
-                                     y))
 
 @route GET "/streams"
 (defun render-streams (&key |archived| (|year| (current-year)))
@@ -113,24 +113,26 @@
 ;; Throw 400 if the stream is not streaming
 @route POST "/stream/is_streaming_nginx"
 (defun stream-is-streaming (&key |name|)
-  (usufslc.db:with-db ()
-                      (let ((stream (mito:find-dao 'usufslc.db.vidstream:vidstream :id |name|)))
-                        (if (and stream
-                                 (usufslc.db.vidstream::vidstream-streaming stream))
-                          "Success"
-                          (throw-code 400)))))
+  (usufslc.db:with-db
+    ()
+    (let ((stream (mito:find-dao 'usufslc.db.vidstream:vidstream :id |name|)))
+      (if (and stream
+               (usufslc.db.vidstream::vidstream-streaming stream))
+        "Success"
+        (throw-code 400)))))
 
 @route POST "/stream/start_by_token_nginx"
 (defun start-stream-via-token (&key |token| |name|)
-  (usufslc.db:with-db ()
-                      (let ((stream (usufslc.db.vidstream:get-stream-unless-expired |token|)))
-                        (if (and stream
-                                 (= (parse-integer |name|) (mito:object-id stream)))
-                          (progn
-                            (setf (usufslc.db.vidstream::vidstream-streaming stream) t)
-                            (mito:save-dao stream)
-                            "Started stream")
-                          (throw-code 400)))))
+  (usufslc.db:with-db
+    ()
+    (let ((stream (usufslc.db.vidstream:get-stream-unless-expired |token|)))
+      (if (and stream
+               (= (parse-integer |name|) (mito:object-id stream)))
+        (progn
+          (setf (usufslc.db.vidstream::vidstream-streaming stream) t)
+          (mito:save-dao stream)
+          "Started stream")
+        (throw-code 400)))))
 
 ;; prevent a race condition where nginx might send done_recording while
 ;; still setting the stream to have ended, causing the recorded field
@@ -139,18 +141,21 @@
 
 @route POST "/stream/end_by_token_nginx"
 (defun end-stream (&key |token|)
-  (bt:with-lock-held (*update-stream-end-lock*)
-                     (usufslc.db:with-db ()
-                                         (let ((stream (mito:find-dao 'usufslc.db.vidstream:vidstream :token |token|)))
-                                           (when stream
-                                             (usufslc.db.vidstream:rotate-token-and-set-streaming stream nil))))))
+  (bt:with-lock-held
+    (*update-stream-end-lock*)
+    (usufslc.db:with-db
+      ()
+      (let ((stream (mito:find-dao 'usufslc.db.vidstream:vidstream :token |token|)))
+        (when stream
+          (usufslc.db.vidstream:rotate-token-and-set-streaming stream nil))))))
 
 @route POST "/stream/done_recording_nginx"
-(defun set-recorded (&key |name|)
-  (bt:with-lock-held (*update-stream-end-lock*)
-                     (usufslc.db:with-db ()
-                                         (let ((stream (mito:find-dao 'usufslc.db.vidstream:vidstream :id |name|)))
-                                           (when stream
-                                             (setf (usufslc.db.vidstream::vidstream-recorded stream) t)
-                                             (mito:save-dao stream)
-                                             "Saved recording status")))))
+(defun finish-recording (&key |name|)
+  (bt:with-lock-held
+    (*update-stream-end-lock*)
+    (usufslc.db:with-db
+      ()
+      (let ((stream (mito:find-dao 'usufslc.db.vidstream:vidstream :id |name|)))
+        (when stream
+          (setf (usufslc.db.vidstream::vidstream-recorded stream) t)
+          (mito:save-dao stream))))))
